a04ef8f1a2de345d54990e292db84be439040cdf
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-04-11 14:09:02 -0400

    first commit, setup express server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+
+app.use(cors());
+const server = http.createServer(app);
+
+server.listen(3001, () => {
+  console.log("LISTENING SERVER 3001");
+});
