357546e8f86f5502adb97621fc4b58e27ba9e55b
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-04-11 14:46:00 -0400

    connect react with socket io server

--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ const server = http.createServer(app);
 //connect server with socket io
 const io = new Server(server, {
   cors: {
-    origin: "https://localhost:3002",
+    origin: "http://localhost:3002",
     method: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  console.log(`User ${socket.id} connected`);
 
   socket.on("disconnect", () => {
     console.log(`User ${socket.id} disconnected`);
