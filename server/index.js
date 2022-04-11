c80c427dca3dfe58b5b3c0a3a7a7558104e01eea
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-04-11 14:21:43 -0400

    connect server with socket io

--- server/index.js
+++ server/index.js
@@ -2,9 +2,26 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 app.use(cors());
 const server = http.createServer(app);
+
+//connect server with socket io
+const io = new Server(server, {
+  cors: {
+    origin: "https://localhost:3000",
+    method: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id} disconnected`);
+  });
+});
 
 server.listen(3001, () => {
   console.log("LISTENING SERVER 3001");
