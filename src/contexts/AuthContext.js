bbb22152060b5e783db86972e20b71c65fd66369
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-04-18 14:42:15 -0400

    add usecontext

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,29 @@
+import React, { useContet, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useContext } from "react/cjs/react.production.min";
+import { auth } from "../firebase";
+
+const AuthContext = React.createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState({});
+  const history = useHistory();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+      history.push("/chats");
+    });
+  }, [user, history]);
+
+  const value = { user };
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
